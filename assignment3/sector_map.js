01e550fd5abed07f134a1c66d22f2ca6f35cf68a
Ian larson <94594696+CursedFaey@users.noreply.github.com>
2023-05-27 19:27:59 -0700

    base sectors with elements

--- assignment3/sector_map.js
+++ assignment3/sector_map.js
@@ -1,2 +1,51 @@
 // creates a map of the sectors of Valantis (or current city) with their unique names
-//start here
+function scan_sectors(){
+    // Air Sectors
+    output = "Air Sectors: <br> <ol>";
+    sector_data = {
+        "name"      : ["", ""],
+        "details"   : ["", ""],
+        "origin"    : "#name# - #details#"
+    }
+    for (let i = 0; i < 5; i++){
+        output += "<li>" + grammars.GenerationSimple(territory_data) + "</li>";
+    }
+    output += "</ol>";
+
+    // Earth Sectors
+    output = "Earth Sectors: <br> <ol>";
+    sector_data = {
+        "name"      : ["", ""],
+        "details"   : ["", ""],
+        "origin"    : "#name# - #details#"
+    }
+    for (let i = 0; i < 5; i++){
+        output += "<li>" + grammars.GenerationSimple(territory_data) + "</li>";
+    }
+    output += "</ol>";
+
+    // Fire Sectors
+    output = "Fire Sectors: <br> <ol>";
+    sector_data = {
+        "name"      : ["", ""],
+        "details"   : ["", ""],
+        "origin"    : "#name# - #details#"
+    }
+    for (let i = 0; i < 5; i++){
+        output += "<li>" + grammars.GenerationSimple(territory_data) + "</li>";
+    }
+    output += "</ol>";
+
+    // Water Sectors
+    output = "Water Sectors: <br> <ol>";
+    sector_data = {
+        "name"      : ["", ""],
+        "details"   : ["", ""],
+        "origin"    : "#name# - #details#"
+    }
+    for (let i = 0; i < 5; i++){
+        output += "<li>" + grammars.GenerationSimple(territory_data) + "</li>";
+    }
+    output += "</ol>";
+    io.write_into_element(output, "sectors");
+}
